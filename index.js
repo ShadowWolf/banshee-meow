54577b829bb55e913e4a57c9df52945368b8e7e1
Bryan <shadowwolf007@gmail.com>
2018-05-29 18:51:14 -0500

    Set cache age and disable RSVP temporarily so I can deploy

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express', '4.16.2');
 const compression = require('compression');
 const app = express();
 
-app.use(express.static('public'));
+app.use(express.static('public', {maxAge: '10 days'}));
 app.use(compression());
 
 const portNumber = process.env.PORT || 1500;
